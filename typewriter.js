5ac933bac862489f6b46b40e9ee7f8065e41f266
Karamvir-Bains <kbains2000@gmail.com>
2022-10-14 16:46:10 -0600

    Added typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "hello there from lighthouse labs";
+const stdoutDelay = sentence.length * 50;
+let delay = 0;
+
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 50;
+}
+
+setTimeout(() => {
+  process.stdout.write("\n");
+}, stdoutDelay);
